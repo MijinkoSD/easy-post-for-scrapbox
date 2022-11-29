8efe25ce471c3cd2061f42d3207f1efcf056370a
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2022-11-29 16:54:35 +0900

    Add function for CSS management

---
+++ setting.ts
@@ -0,0 +1,9 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="es2022"/>
+
+/**
+ * ハードコーディングされている値をまとめるためのファイル
+ */
+
+
+export const DEFAULT_FORM_ID = "easy-post-for-scrapbox"
--- ui.ts
+++ ui.ts
@@ -3,15 +3,16 @@
 /// <reference lib="dom"/>
 
 import { NoMatchSelectorError } from "./exception.ts";
+import { DEFAULT_FORM_ID } from "./setting.ts";
 
 /**
  * 投稿フォームを描画します。
  * @param {string} [parentDOMSelector="app row-flex"] UIの親となるDOMを指定する際に使用するセレクターを指定します。
- * @param {string} [formId="easy-post-for-scrapbox"] 投稿フォームのDOMに設定するID属性の値。
+ * @param {string} [formId=DEFAULT_FORM_ID] 投稿フォームのDOMに設定するID属性の値。
  */
 export function renderPostForm(
   parentDOMSelector = ".app .row-flex",
-  formId = "easy-post-for-scrapbox",
+  formId = DEFAULT_FORM_ID,
 ) {
   const parent = document.querySelector(parentDOMSelector);
   if (parent === null) throw new NoMatchSelectorError(parentDOMSelector);
@@ -21,4 +22,33 @@ export function renderPostForm(
   const sendButton = document.createElement("button");
   root.append(textarea, sendButton);
   parent.appendChild(root);
+}
+
+/**
+ * CSSを適用します。
+ */
+export function enableCSS(cssSrc: string) {
+  const existLinks = document.head.getElementsByTagName("link");
+  for (const i in existLinks) {
+    if (existLinks[i].getAttribute("href") == cssSrc) {
+      return;
+    }
+  }
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = cssSrc;
+  document.head.appendChild(link);
+}
+
+/**
+ * CSSを無効化します。
+ */
+export function removeCSS(cssSrc: string) {
+  const existLinks = document.head.getElementsByTagName("link");
+  for (const i in existLinks) {
+    if (existLinks[i].getAttribute("href") == cssSrc) {
+      existLinks[i].remove();
+    }
+  }
 }
