8c2de5e64f2370765bf0a67ff3bf09d3d7f9438c
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2022-12-13 19:12:54 +0900

    Change dependencies
    mod.tsから読み込んでいたものを個別のファイルから読み込むように変更した。
    それにより、bundle後のファイルサイズが少し減った。

--- deps/scrapbox-userscript-std.ts
+++ deps/scrapbox-userscript-std.ts
@@ -3,8 +3,10 @@ export type {
 } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.10/deps/scrapbox-rest.ts";
 export type {
   Socket,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.10/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.10/browser/websocket/socket.ts";
 export {
   makeSocket,
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.10/browser/websocket/socket.ts";
+export {
   patch,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.10/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.10/browser/websocket/patch.ts";
